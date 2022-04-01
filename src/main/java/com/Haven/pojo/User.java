fb33e9cd8746f91bf2f6e32bd692387023ff8da6
JustHoldWay <88335815+Haven-jiang@users.noreply.github.com>
2022-04-01 19:08:07 +0800

    init

--- src/main/java/com/Haven/pojo/User.java
+++ src/main/java/com/Haven/pojo/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -14,6 +15,12 @@ public class User implements Serializable {
     private Integer id;
     private String username;
     private String password;
+    private Integer loginType;
+    private String ipAddress;
+    private String ipSource;
     private String prems;
     private String e_mail;
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+    private LocalDateTime lastLoginTime;
 }
